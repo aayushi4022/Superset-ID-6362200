3c19aa18300ab0401f193adde6397102d2f33ab9
aayushi4022 <144204020+aayushi4022@users.noreply.github.com>
2025-08-04 07:10:58 +0530

    Add files via upload

--- App.js
+++ App.js
@@ -1,20 +1,10 @@
 import React from 'react';
-import Counter from './components/Counter';
-import SayWelcome from './components/SayWelcome';
-import OnPressButton from './components/OnPressButton';
-import CurrencyConverter from './components/CurrencyConverter';
+import OfficeList from './components/OfficeList';
 
 function App() {
   return (
-    <div style={{ padding: '20px' }}>
-      <h1>Event Handling Examples</h1>
-      <Counter />
-      <hr />
-      <SayWelcome />
-      <hr />
-      <OnPressButton />
-      <hr />
-      <CurrencyConverter />
+    <div className="App">
+      <OfficeList />
     </div>
   );
 }
